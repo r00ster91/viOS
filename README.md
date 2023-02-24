5d74e0ce1e03e54f53554aaed5689c30c93d2998
r00ster91 <r00ster91@proton.me>
2023-02-24 19:22:27 +0100

    finish off
    * add demo video
    * more documentation
    * implement 'W' motion

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # viOS
 
+https://user-images.githubusercontent.com/35064754/221260670-3c2df571-ca44-4985-b3cc-175a59e526aa.mp4
+
 *And you thought vi couldn't be an operating system, Emacs user!*
+
+viOS is an operating system that runs vi, in form of a BIOS bootloader.
+
+Supported vi motions:
+
+* <kbd>h</kbd>
+* <kbd>l</kbd>
+* <kbd>W</kbd>
+* <kbd>0</kbd>, <kbd>$</kbd>
+* <kbd>_</kbd>
+* <kbd>x</kbd>, <kbd>X</kbd>
+* <kbd>i</kbd>, <kbd>a</kbd>
+* <kbd>I</kbd>, <kbd>A</kbd>
+
+## Running
+
+Requires NASM and QEMU.
+
+```
+./run.sh
+```
+
+## Known bugs and limitations
+
+* Cursor corrupts when pressing <kbd>W</kbd> and no space follows.
+* No multiline support; I quickly ran out of bytes and couldn't add any more features.
--- main.s
+++ main.s
@@ -1,40 +1,36 @@
-[bits 16] ; real mode
-[org 0x7c00] ; load address
+[bits 16] ; Real mode
+[org 0x7c00] ; Load address
 
 ;
-; normal mode
+; Normal mode
 ;
 
 run_normal_mode:
     call update_fn
 
-    ; make the cursor fill the cell
+    ; Make the cursor fill the cell.
     mov ah, 0x01
     xor ch, ch
     mov cl, 0b0001_0000
     int 0x10
     
-    ; read keypress (blocking)
+    ; Read keypress (blocking).
     xor ah, ah
     int 0x16
-    ; cursor movement
+    ; Cursor movement
     cmp al, 'h'
     je move_cursor_left
     cmp al, 'l'
     je move_cursor_right
-    ;cmp al, 'w' ; specific separators
-    ;je advance_cursor_by_word_sep
-    ;cmp al, 'W' ; only space as separator
-    ;je advance_cursor_by_word_space
-    ;cmp al, 'd'
-    ;je delete_
+    cmp al, 'W'
+    je advance_cursor_by_word
     cmp al, '0'
     je set_cursor_to_start
     cmp al, '$'
     je set_cursor_to_end
     cmp al, '_'
     je set_cursor_to_before_first_word
-    ; text modification
+    ; Text modification
     cmp al, 'x'
     je delete_char_after_cursor
     cmp al, 'X'
@@ -51,92 +47,118 @@ run_normal_mode:
     jmp run_normal_mode
 
 move_cursor_left:
-    dec byte [cursor_pos.col]
-    ; if cursor column underflowed, set to zero
+    dec byte [cursor_col]
+    ; If cursor column underflowed, set to zero.
     jns run_normal_mode
-    mov byte [cursor_pos.col], 0
+    mov byte [cursor_col], 0
     jmp run_normal_mode
 move_cursor_right:
     mov dl, [buf_len]
-    ; if line is empty, do nothing
+    ; If line is empty, do nothing.
     or dl, dl
     je run_normal_mode
-    inc byte [cursor_pos.col]
+    inc byte [cursor_col]
     dec dl
-    ; if cursor column exceeds buffer length, set to buffer length
-    cmp byte [cursor_pos.col], dl
+    ; If cursor column exceeds buffer length, set to buffer length.
+    cmp byte [cursor_col], dl
     jng run_normal_mode
-    mov byte [cursor_pos.col], dl
+    mov byte [cursor_col], dl
     jmp run_normal_mode
+advance_cursor_by_word:
+    mov si, buf
+    add si, [cursor_col]
+    ; If we're on a space, go to the next non-space.
+    cmp byte [cursor_col], ' '
+    jne go_to_next_space
+go_to_next_non_space:
+    call set_cursor_to_before_next_non_space_fn
+    add byte [cursor_col], cl
+    inc byte [cursor_col]
+    jmp run_normal_mode
+    ; If we're on a non-space, go to the next space and then to the next non-space.
+go_to_next_space:
+    mov cl, -1
+.loop:
+    inc cl
+    lodsb ; Load (DS:SI) into AL and increment SI.
+    cmp al, ' '
+    jne .loop
+.end:
+    add [cursor_col], cl
+    jmp go_to_next_non_space
+    
 set_cursor_to_start:
-    mov byte [cursor_pos.col], 0
+    mov byte [cursor_col], 0
     jmp run_normal_mode
 set_cursor_to_end:
     mov dl, [buf_len]
     dec dl
-    mov byte [cursor_pos.col], dl
+    mov byte [cursor_col], dl
     jmp run_normal_mode
 set_cursor_to_before_first_word:
-    call set_cursor_to_before_first_word_fn
+    mov si, buf
+    call set_cursor_to_before_next_non_space_fn
+    mov byte [cursor_col], cl
     jmp run_normal_mode
 delete_char_after_cursor:
-    ; if buffer length is zero, do nothing
+    ; If buffer length is zero, do nothing.
     cmp byte [buf_len], 0
     je run_normal_mode
-    ; start at cursor column
+    ; Start at cursor column.
     mov si, buf
-    add si, [cursor_pos.col]
+    add si, [cursor_col]
     call delete_char_at_cursor_col_fn
     jmp run_normal_mode
 delete_char_before_cursor:
-    ; if cursor column is zero, do nothing
-    cmp byte [cursor_pos.col], 0
+    ; If cursor column is zero, do nothing.
+    cmp byte [cursor_col], 0
     je run_normal_mode
-    ; if buffer length is zero, do nothing
+    ; If buffer length is zero, do nothing.
     cmp byte [buf_len], 0
     je run_normal_mode
-    ; start at cursor column
+    ; Start at cursor column.
     mov si, buf
-    add si, [cursor_pos.col]
+    add si, [cursor_col]
     dec si
     call delete_char_at_cursor_col_fn
-    dec byte [cursor_pos.col]
+    dec byte [cursor_col]
     jmp run_normal_mode
 insert_after_cursor:
-    inc byte [cursor_pos.col]
-    ; if cursor column exceeds buffer length, set to buffer length
+    inc byte [cursor_col]
+    ; If cursor column exceeds buffer length, set to buffer length.
     mov dl, [buf_len]
-    cmp byte [cursor_pos.col], dl
+    cmp byte [cursor_col], dl
     jng run_insert_mode
-    mov byte [cursor_pos.col], dl
+    mov byte [cursor_col], dl
     jmp run_insert_mode
 insert_before_first_word:
-    call set_cursor_to_before_first_word_fn
+    mov si, buf
+    call set_cursor_to_before_next_non_space_fn
+    mov byte [cursor_col], cl
     jmp run_insert_mode
 insert_at_end:
     mov dl, [buf_len]
-    mov byte [cursor_pos.col], dl
+    mov byte [cursor_col], dl
     jmp run_insert_mode
 
-set_cursor_to_before_first_word_fn:
-    mov si, buf
+; SI holds the buffer.
+set_cursor_to_before_next_non_space_fn:
     mov cl, -1
 .find_non_space:
     inc cl
-    lodsb ; load (DS:SI) into AL and increment SI
+    lodsb ; Load (DS:SI) into AL and increment SI.
     cmp al, ' '
     je .find_non_space
 .end:
-    mov [cursor_pos.col], cl
     ret
 
 delete_char_at_cursor_col_fn:
 .loop:
-    ; take the next character
+    ; Take the next character.
     mov dl, [si + 1]
-    ; and move it to the current
+    ; And move it to the current.
     mov [si], dl
-    ; check if we're at the end
+    ; Check if we're at the end.
     mov bx, buf
     add bx, [buf_len]
     cmp si, bx
@@ -144,53 +166,53 @@ delete_char_at_cursor_col_fn:
     jc .loop
 .end:
     dec byte [buf_len]
-    ; if cursor column matches buffer length, move cursor to the left
-    mov dl, [cursor_pos.col]
+    ; If cursor column matches buffer length, move cursor to the left.
+    mov dl, [cursor_col]
     cmp dl, [buf_len]
     jne run_normal_mode
-    dec byte [cursor_pos.col]
-    ; if cursor column underflowed, set to zero
+    dec byte [cursor_col]
+    ; If cursor column underflowed, set to zero.
     jns run_normal_mode
-    mov byte [cursor_pos.col], 0
+    mov byte [cursor_col], 0
     ret
 
 ;
-; insert mode
+; Insert mode.
 ;
 
 run_insert_mode:
     call update_fn
 
-    ; make the cursor fill the bottom quarter of the cell
+    ; Make the cursor fill the bottom quarter of the cell.
     mov ah, 0x01
     mov ch, 0b0000_1011
     mov cl, 0b0001_1111
     int 0x10
 
-    ; read keypress (blocking)
+    ; Read keypress (blocking).
     xor ah, ah
     int 0x16
-    ; evaluate input
-    cmp al, `\e` ; escape
+    ; Evaluate input.
+    cmp al, `\e` ; Escape
     je enter_normal_mode
-    cmp al, `\t` ; tab
+    cmp al, `\t` ; Tab
     je insert_four_spaces
-    ; ignore input that would corrupt state
-    cmp al, `\b` ; backspace
+    ; Ignore input that would corrupt state.
+    cmp al, `\b` ; Backspace
     je run_insert_mode
-    or al, al ; delete
+    or al, al ; Delete
     je run_insert_mode
-    cmp al, `\r` ; enter
+    cmp al, `\r` ; Enter
     je run_insert_mode
 
     call insert_char_fn
     jmp run_insert_mode
 
 enter_normal_mode:
-    dec byte [cursor_pos.col]
-    ; on underflow, set to zero
+    dec byte [cursor_col]
+    ; On underflow, set to zero.
     jns run_normal_mode
-    mov byte [cursor_pos.col], 0
+    mov byte [cursor_col], 0
     jmp run_normal_mode
 insert_four_spaces:
     mov al, ' '
@@ -200,67 +222,64 @@ insert_four_spaces:
     call insert_char_fn
     jmp run_insert_mode
 
-    ; insert the character in AL
+    ; Insert the character in AL.
 insert_char_fn:
-    ; start at the end of the buffer
+    ; Start at the end of the buffer.
     mov si, buf
     add si, [buf_len]
     dec si
 .loop:
-    ; take the current character
+    ; Take the current character.
     mov dl, [si]
-    ; and move it to the next
+    ; And move it to the next.
     mov [si + 1], dl
-    ; check if we're at the end
+    ; Check if we're at the end.
     mov bx, buf
-    add bx, [cursor_pos.col]
+    add bx, [cursor_col]
     cmp si, bx
     dec si
     jnc .loop
 .end:
-    ; increase buffer length, set character, and advance cursor position
+    ; Increase buffer length, set character, and advance cursor position.
     inc byte [buf_len]
     mov [si + 2], al
-    inc byte [cursor_pos.col]
+    inc byte [cursor_col]
     ret
 
 ;
-; presentation
+; Presentation
 ;
 
 update_fn:
-    ; set screen resolution to 80x25, clear screen, and set cursor position to (0, 0)
+    ; Set screen resolution to 80x25, clear screen, and set cursor position to (0, 0).
     xor ah, ah
     mov al, 2
     int 0x10
 
-    ; update screen content
+    ; Update screen content.
     mov si, buf
-    mov ah, 0x0e ; set up for writing
+    mov ah, 0x0e ; Set up for writing.
     mov cl, -1
 .loop:
     inc cl
-    lodsb ; load (DS:SI) into AL and increment SI
-    int 0x10 ; print AL
+    lodsb ; Load (DS:SI) into AL and increment SI.
+    int 0x10 ; Print AL.
     cmp cl, [buf_len]
     jne .loop
 .end:
-    ; set cursor position
+    ; Set cursor position.
     mov ah, 0x02
-    xor bh, bh ; page number
-    ;mov dh, [cursor_pos.row]
-    xor dh, dh ; row
-    mov dl, [cursor_pos.col] ; column
+    xor bh, bh ; Page number
+    xor dh, dh ; Row
+    mov dl, [cursor_col] ; Column
     int 0x10
     ret
 
 buf_len: db 0
-cursor_pos:
-    ;.row: db 0
-    .col: db 0
+cursor_col: db 0
 
-; fill the rest of the sector with zeroes to make it exactly 0x200 bytes big
+; Fill the rest of the sector with zeroes to make it exactly 0x200 bytes big.
 times 0x200 - 2 - ($ - $$) db 0
-dw 0xaa55 ; magic word
+dw 0xaa55 ; Magic word
 
 buf:
